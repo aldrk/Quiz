773dc623d41e32a1d0e7cbad1a46bce03a813d78
Alexandr <al.krupoderov@gmail.com>
2021-01-30 14:03:12 +0300

    husky check

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React from "react";
 
-const App: React.FC = () => <div>React App</div>
+const App: React.FC = () => {
+  return <div>React App</div>
+}
 
 export default App
